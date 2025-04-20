497b091bbe8ea6097d54328183d5a8d09bfc421e
HarshVardhan-07 <harshvardhan0655@gmail.com>
2025-04-20 14:50:52 +0530

    Added Images for items

--- backend/seed.js
+++ backend/seed.js
@@ -18,30 +18,31 @@ const seedMenu = async () => {
       category: 'Main Course',
       description: 'Classic cheese and tomato pizza.',
       price: 250,
-      imageUrl: '',
+      imageUrl: 'https://i.pinimg.com/736x/57/e7/a1/57e7a19334571946391a7430fcb86202.jpg',
     },
     {
       name: 'Coke',
       category: 'Drinks',
       description: 'Chilled Coca-Cola bottle (500ml)',
       price: 50,
-      imageUrl: '',
+      imageUrl: 'https://www.coca-cola.com/content/dam/onexp/us/en/brands/coca-cola-multi-brand/coke-category-logos/v2/coca-cola.jpg',
     },
     {
       name: 'Chocolate Lava Cake',
       category: 'Desserts',
       description: 'Warm chocolate cake with a gooey center.',
       price: 120,
-      imageUrl: '',
+      imageUrl: 'https://bakewithshivesh.com/wp-content/uploads/2022/09/IMG_0484-scaled.jpg',
     },
     {
       name: 'Fries',
       category: 'Appetizers',
       description: 'Crispy golden fries.',
       price: 80,
-      imageUrl: '',
-    }
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/8/83/French_Fries.JPG',
+    },
   ];
+  
 
   await MenuItem.deleteMany(); // Clear existing data
   await MenuItem.insertMany(items);
